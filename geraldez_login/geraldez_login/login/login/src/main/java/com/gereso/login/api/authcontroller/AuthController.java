08c2e0cc2994846a57e2ef15f464d2bc2b920b2c
Exoltl <esplaguerajayd@gmail.com>
2024-07-16 21:15:16 +0800

    added CrossOrigin

--- geraldez_login/geraldez_login/login/login/src/main/java/com/gereso/login/api/authcontroller/AuthController.java
+++ geraldez_login/geraldez_login/login/login/src/main/java/com/gereso/login/api/authcontroller/AuthController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +22,7 @@ import com.gereso.login.userservice.UserService;
 
 import springfox.documentation.annotations.ApiIgnore;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class AuthController {
 
